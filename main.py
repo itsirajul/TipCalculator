a138514f71fe95e97d58fa11bd66395c69b0401f
itsirajul <shirajulislamparvez@gmail.com>
2022-09-07 04:22:43 +0600

    Type Casting Practice

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Welcome to Tip Calculator")
+two_digit_number = input("Type Two Digit Number:")
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+result = int(first_digit)+int(second_digit)
+print(result)
