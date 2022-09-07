5b46ac0cf3535c4d3254dbdefa2c2024af683169
itsirajul <shirajulislamparvez@gmail.com>
2022-09-07 10:19:50 +0600

    Three Digit Practice

---
+++ desimal print.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Enter Any Three Digit Number: ")
+
+first_number = two_digit_number[0]
+second_number = two_digit_number[2]
+third_number = two_digit_number[4]
+result = int(first_number)+int(second_number)+int(third_number)
+print(result)
